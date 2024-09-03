a2d708ce9f57fbec25e9217b60ec9226a4f4e4b3
Anton1802 <anton.sup1802@gmail.com>
2024-09-03 11:40:54 +0300

    Add signin and signup forms

--- app/views.py
+++ app/views.py
@@ -1,8 +1,17 @@
 from app import app
-from flask               import render_template
+from flask import render_template
 
 # Index
-@app.route('/', defaults={'path': 'index.html'})
-@app.route('/path')
-def index(path: str) -> str:
-    return render_template(path)
+@app.route('/')
+def index() -> str:
+    return render_template('index.html')
+
+# SignIn
+@app.route('/signin')
+def signin() -> str:
+    return render_template('signin.html')
+
+# SignUp
+@app.route('/signup')
+def signup() -> str:
+    return render_template('signup.html')
