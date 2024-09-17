8e73505eedc3c21689abf61662492e47ce672ae9
Anton <98132139+Anton1802@users.noreply.github.com>
2024-09-17 19:15:31 +0500

    Run project settings

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ basedir: str = os.path.abspath(os.path.dirname(__file__))
 
 class Config():
     CSRF_ENABLED = True
-    SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_007')
+    SECRET_KEY = config('SECRET_KEY')
 
     SQLALCHEMY_DATABASE_URI = config('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
