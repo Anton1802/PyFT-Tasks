1c585087f1a37011fcd904c25d005ad11b8f5c4a
Anton <98132139+Anton1802@users.noreply.github.com>
2024-09-26 16:55:22 +0500

    refactory send_message()

--- app/util.py
+++ app/util.py
@@ -1,14 +1,7 @@
-import requests
 from decouple import config
+from telebot import TeleBot
 
 def send_message(message: str, chat_id):
     TOKEN_TELEGRAM_BOT = config("TOKEN_TELEGRAM_BOT")
-    URL = f'https://api.telegram.org/bot{TOKEN_TELEGRAM_BOT}/sendMessage'
-    payload = {
-        'chat_id': chat_id,
-        'text': message,
-    }
-    
-    response = requests.post(URL, data=payload, timeout=5)
-
-    return response.json()
+    bot = TeleBot(token=TOKEN_TELEGRAM_BOT)
+    bot.send_message(chat_id, message, parse_mode='Markdown')
