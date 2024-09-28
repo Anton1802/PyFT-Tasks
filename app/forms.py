b359208391cbb412fad437cede8a30c2ba1e0498
Anton <98132139+Anton1802@users.noreply.github.com>
2024-09-29 00:11:18 +0500

    edit validators rules

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,6 @@ from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, Length
 
 class UpandInForm(FlaskForm):
-    username: StringField = StringField('Username', validators=[DataRequired(), Length(1, 20)])
-    password: PasswordField = PasswordField('Password', validators=[DataRequired(), Length(8, 150)])
+    username: StringField = StringField('Username', validators=[DataRequired(), Length(4, 25)])
+    password: PasswordField = PasswordField('Password', validators=[DataRequired()])
     submit: SubmitField = SubmitField()
